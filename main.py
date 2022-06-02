15c4962328daa42ba7914deb2469e426a035e66d
EvgenMarfel <mormarfel2@gmail.com>
2022-06-02 09:15:16 +0300

    initian project

---
+++ main.py
@@ -0,0 +1,17 @@
+import data_file
+
+profit = 0
+is_on = True
+
+while is_on:
+    question = input("What would you like?: \n1: espresso\n2: latte\n3: cappuccino")
+    if question == 'off':
+        is_on = False
+    elif question == "report":
+        print(f'water: {data_file.resources["water"]} ml')
+        print(f'milk: {data_file.resources["milk"]} ml')
+        print(f'coffee: {data_file.resources["coffee"]} g')
+        print(f'profit is: ${profit}')
+    else:
+        drink = data_file.MENU[question]
+
